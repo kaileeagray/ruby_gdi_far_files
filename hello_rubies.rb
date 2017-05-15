50147fab44f9bab70a17b1b52441cdb86bc56226
Kailee <kaileeagray@gmail.com>
2017-05-15 14:48:29 -0500

    add hello_rubies

---
+++ hello_rubies.rb
@@ -0,0 +1,22 @@
+# Ruby program to greet all attendees of Girl Develop It Fargo"s Intro to Ruby class.
+
+# First, build arrays of all attendees. Assign each array to a variable.
+gdi_far_leaders = ["Shannon", "Megan"]
+helpers = ["Brian", "Matt", "Neils", "Baron"]
+learners = ["Katy", "Hannah", "Shiny", "Karena", "Chris", "jango0019", "Linda", "Amanda", "Susan"]
+
+# Display a welcome message to learners.
+learners.each do |learner|
+  puts "Hello, #{learner}! Welcome to Intro to Ruby with Girl Develop It Fargo!"
+end
+
+# Display a welcome message to gdi_leaders.
+gdi_far_leaders.each do |gdi_leader|
+  puts "Hey, #{gdi_leader}! You rock!"
+end
+
+# Display a welcome message to gdi_leaders.
+helpers.each do |helper|
+  puts "Yo, #{helper}! Thanks for helping! You rock!"
+end
+
