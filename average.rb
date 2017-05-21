3f076d68cefdccf8761aa883c225525bc3ec46f0
Kailee <kaileeagray@gmail.com>
2017-05-21 18:31:50 -0500

    Add hash activity, readme

---
+++ average.rb
@@ -0,0 +1,9 @@
+numbers_dev = [23, 6, 47, 35, 2, 14]
+
+sum = 0
+
+numbers_dev.each do |number|
+  sum += number
+end
+
+puts "sum is #{sum}"
---
+++ ruby_far_class.rb
@@ -0,0 +1,52 @@
+# Ruby program to greet all attendees of Girl Develop It Fargo's Intro to Ruby class.
+
+# First, build arrays of all attendees. Assign each array to a variable.
+gdi_far_leaders = ["Shannon", "Megan"]
+helpers = ["Brian", "Kailee"]
+learners = ["Katy", "Hannah", "Shiny", "Karena", "Chris", "jango0019", "Linda", "Amanda", "Susan"]
+
+# Let's change learners into a hash!
+# We're going to use hash rocket syntax learner_info = {"name" => "favorite"}. 
+# To access value, use learner_info["name"]
+
+# OR Google more about symbols in Ruby to use this syntax learner_info = {name: "favorite"}
+# To access value, use learner_info[:name]
+
+# Note key cannot be numeric.
+# Help me remember your names! What's your favorite show/movie/book/hobby?
+# I'll start!
+
+gdi_crew_info = {}
+gdi_crew_info["Kailee"] = "bike riding"
+
+puts "Check out our hash!"
+puts gdi_crew_info
+puts "\n"
+
+puts "#{"**Name**".ljust(30, '.')} **Favorite Thing**"
+puts "-"*49
+
+# Display info about everyone here.
+gdi_crew_info.each do |gdi_name, favorite|
+  puts "#{gdi_name.capitalize.ljust(30, '.')} #{favorite}"
+end
+
+# Display a welcome message to learners.
+# learners.each do |learner|
+#   puts "Hello, #{learner}! Welcome to Intro to Ruby with Girl Develop It Fargo!"
+# end
+
+# puts "-"*70
+
+# Display a welcome message to gdi_leaders.
+# gdi_far_leaders.each do |gdi_leader|
+#   puts "Hey, #{gdi_leader}! You rock!"
+# end
+
+# puts "-"*70
+
+# Display a welcome message to gdi_leaders.
+# helpers.each do |helper|
+#   puts "Yo, #{helper}! Thanks for helping!"
+# end
+
