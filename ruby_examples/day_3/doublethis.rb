e02f3e811d2081986c011006944f52dca32c446c
Kailee <kaileeagray@gmail.com>
2017-05-21 20:02:18 -0500

    arrange files

--- ruby_examples/day_3/doublethis.rb
+++ ruby_examples/day_3/doublethis.rb
@@ -1,7 +1,9 @@
+# can you fix this? what's wrong?
+
 def doubleThis num
-  return numTimes2 = num*2
+  numTimes2 = num*2
   puts num.to_s+' doubled is '+numTimes2.to_s
 end
 
-numTimes2 = doubleThis 44
+doubleThis 44
 puts numTimes2.to_s 
---
+++ ruby_examples/day_3/subtract.rb
@@ -0,0 +1,28 @@
+def subtract_one(x,y)
+  x - y
+  'another thing'
+end
+
+return_value = subtract_one(5, 2)
+puts return_value
+
+def subtract(x,y)
+  return x - y
+  'another thing'
+end
+
+return_value = subtract(5, 2)
+puts return_value
+
+def subtract_two(x,y)
+  'another thing'
+   x - y  
+end
+
+return_value = subtract_two(5, 2)
+puts return_value
+
+
+# puts "This is the begining of my program"
+# return_value = subtract(2,3)
+# puts return_value
